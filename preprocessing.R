c32cad5bf16576a4c2c19fe1af27f49e7e920925
lauturgi <lauturgi@alumni.uv.es>
2025-03-06 20:24:10 +0100

    Number of proteins per sample summary + plot before and after filtering by sample proportion | Complex Heatmap - clustering by rows and columns using binary distance - Split by frailty or annotate columns

--- preprocessing.R
+++ preprocessing.R
@@ -7,6 +7,7 @@ library(nortest)
 library(dplyr)
 library(SummarizedExperiment)
 library(reshape2)
+library(ComplexHeatmap)
 #renv::install("bioc::impute")
 #renv::install()
 
@@ -145,8 +146,14 @@ table(is.na(se_assay))
 # FALSE    TRUE 
 # 61378 1329984
 
+# Number of proteins with all NA
+proteins_to_keep <- apply(se_assay, 1, function(x) !all(is.na(x)))
+sum(!proteins_to_keep)
+# 3952 
+
 # Filter proteins with all NA
-se_assay <- se_assay[apply(se_assay, 1, function(x) !all(is.na(x))), ]
+se_assay <- se_assay[proteins_to_keep, ]
+se <- se[proteins_to_keep,]
 
 # Number of proteins after filtering those with all NA
 nrow(se_assay)
@@ -156,6 +163,37 @@ nrow(se_assay)
 table(is.na(se_assay))
 # FALSE   TRUE 
 # 61378 527728 
+
+# Number of proteins detected per sample
+num_prot_sample <- apply(se_assay, 2, function(x) sum(!is.na(x)))
+num_prot_sample <- as.data.frame(num_prot_sample)
+colnames(num_prot_sample) <- "Proteins"
+num_prot_sample$Sample <- substr(rownames(num_prot_sample), 1, 7)
+rownames(num_prot_sample) <- NULL
+# Add frailty group
+num_prot_sample <- merge(num_prot_sample, col_data[, c("sample", "frailty")], 
+                         by.x = "Sample", by.y = "sample")
+
+# Summary total proteins detected per sample
+summary(num_prot_sample)
+#Sample             Proteins      frailty  
+#Length:203         Min.   :  14.0   NFT:138  
+#Class :character   1st Qu.: 183.5   FT : 65  
+#Mode  :character   Median : 288.0            
+#                   Mean   : 302.4            
+#                   3rd Qu.: 378.5            
+#                   Max.   :1101.0  
+
+# Check proteins detected in samples with a low detection
+rownames(se_assay[!is.na(se_assay[,"YDD_248 MaxLFQ.Intensity"]), ])
+
+# Density plot number of proteins per sample in FT vs NFT
+ggplot(num_prot_sample, aes(x = Proteins, fill = frailty)) +
+  geom_density(alpha = 0.4, linewidth = 0.2) +
+  labs(title = "Number of proteins per sample", x = "Number of proteins", y = "Density") +
+  scale_fill_manual(values = c("FT" = "blue", "NFT" = "red")) +
+  theme_minimal()
+
 
 # Number of missing per sample
 num_na_sample <- apply(se_assay, 2, function(x) sum(is.na(x)))
@@ -177,6 +215,7 @@ ggplot(num_na_sample, aes(x = NAs, fill = frailty)) +
 
 # Make binary assay (1 = valid value, 0 = missing value)
 missval <- ifelse(is.na(se_assay), 0, 1)
+colnames(missval) <- substr(colnames(missval), 1, 7)
 
 # Convert matrix to long format dataframe
 df <- melt(missval)
@@ -201,13 +240,34 @@ ggplot(df, aes(sample, protein, fill = intensity)) +
         axis.ticks.y = element_blank()) +
   scale_fill_gradient(low = "white", high = "black")
 
+# Complex Heatmap
+# Clustering rows and columns by binary distance
+# Column split by frailty groups
+Heatmap(missval, name = "NA", col = c("white", "black"), show_row_dend = FALSE, 
+        column_names_gp = gpar(fontsize = 4), show_row_names = FALSE, 
+        # cluster_columns = FALSE,
+        # cluster_rows = FALSE,
+        column_split = colData(se)$frailty, 
+        clustering_distance_rows = "binary", 
+        clustering_distance_columns = "binary")
+
+# Annotation of columns by frailty group
+column_ann <- HeatmapAnnotation(frailty = colData(se)$frailty,
+                                col = list(frailty = c("FT" = "red", 
+                                                       "NFT" = "blue")))
+Heatmap(missval, name = "NA", col = c("white", "black"), show_row_dend = FALSE, 
+        column_names_gp = gpar(fontsize = 4), show_row_names = FALSE, 
+        # cluster_columns = FALSE,
+        # cluster_rows = FALSE,
+        clustering_distance_rows = "binary", 
+        clustering_distance_columns = "binary",
+        top_annotation = column_ann)
+
 # ==================================
-# 4.1. Filtering missing
+# 4.1. Filtering missing by sample proportion
 # ==================================
-#############
-# REVISAR!! #
-#############
-# Percentage of proteins detected per sample
+
+# Sample proportion for each protein
 presence_proportion <- rowSums(!is.na(se_assay)) / ncol(se_assay)
 sample_percent <- presence_proportion * 100
 sample_percent <- data.frame(SamplePercentage = sample_percent)
@@ -215,37 +275,165 @@ create_density_plot(sample_percent, "SamplePercentage")
 
 # Number of proteins present in more than 25%
 sum(presence_proportion > 0.25)
+# 320
 
 # Number of proteins present in more than 50%
 sum(presence_proportion > 0.5)
+# 159
 
 # Number of proteins present in more than 75%
 sum(presence_proportion > 0.75)
+# 72
+
+# Number of proteins present in 100%
+sum(presence_proportion == 1)
+# 3
+
+# Proteins present in 100% of samples
+rownames(se_assay[presence_proportion == 1,])
+# P00761 --> trypsin
+# P04264 --> Keratin, type II cytoskeletal 1
+# P06702 --> Protein S100-A9: regulation of inflammatory processes and immune response
+
+# Proteins in more than 75% of samples
+rownames(se_assay[presence_proportion >= 0.75,])
+
+# Proteins in less than 50% of samples
+rownames(se_assay[presence_proportion < 0.5,])
+
+#########################################################
+# ANALIZAR PROTEINAS PRESENTES EN < 50% DE LAS MUESTRAS #
+#########################################################
 
 # Keep proteins with a minimum fraction of valid values in at least one condition
 min_fraction <- 0.5
-fraction_ft <- apply(data_matrix[,colData(se)$frailty == "FT"], 1,
+fraction_ft <- apply(se_assay[,colData(se)$frailty == "FT"], 1,
                      function(x) sum(!is.na(x)) / length(x))
-fraction_nft <- apply(data_matrix[, colData(se)$frailty == "NFT"], 1,
+fraction_nft <- apply(se_assay[, colData(se)$frailty == "NFT"], 1,
                       function(x) sum(!is.na(x)) / length(x))
 
 proteins_to_keep <- (fraction_ft >= min_fraction) | (fraction_nft >= 
                                                        min_fraction)
-sum(proteins_to_keep) # 185
+
+# Number of proteins present in >= 50% of samples in at least one condition
+sum(proteins_to_keep) 
+# 185
 
 # Filter SummarizedExperiment
 se_filtered <- se[proteins_to_keep, ]
-data_matrix <- assay(se_filtered)
 
-dim(se_filtered)
+# Get protein intensities matrix from filtered SummarizedExperiment
+se_filtered_assay <- assay(se_filtered)
 
-sum(is.na(assay(se_filtered)))
+# Number of proteins after filtering those present in <50% of samples in both
+# conditions
+nrow(se_filtered_assay)
+# 185
+
+# Number of proteins detected per sample
+num_prot_sample <- apply(se_filtered_assay, 2, function(x) sum(!is.na(x)))
+num_prot_sample <- as.data.frame(num_prot_sample)
+colnames(num_prot_sample) <- "Proteins"
+num_prot_sample$Sample <- substr(rownames(num_prot_sample), 1, 7)
+rownames(num_prot_sample) <- NULL
+# Add frailty group
+num_prot_sample <- merge(num_prot_sample, col_data[, c("sample", "frailty")], 
+                         by.x = "Sample", by.y = "sample")
+
+# Summary total proteins detected per sample
+summary(num_prot_sample)
+#Sample             Proteins     frailty  
+#Length:203         Min.   : 14.0   NFT:138  
+#Class :character   1st Qu.:103.0   FT : 65  
+#Mode  :character   Median :140.0            
+#                   Mean   :127.6            
+#                   3rd Qu.:161.5            
+#                   Max.   :182.0  
+
+# Density plot number of proteins per sample in FT vs NFT
+ggplot(num_prot_sample, aes(x = Proteins, fill = frailty)) +
+  geom_density(alpha = 0.4, linewidth = 0.2) +
+  labs(title = "Number of proteins per sample", x = "Number of proteins", y = "Density") +
+  scale_fill_manual(values = c("FT" = "blue", "NFT" = "red")) +
+  theme_minimal()
+
+
+# Number of NA values in assay
+table(is.na(se_filtered_assay))
+# FALSE  TRUE 
+# 25894 11661 
+
+# Number of missing per sample
+num_na_sample <- apply(se_filtered_assay, 2, function(x) sum(is.na(x)))
+num_na_sample <- as.data.frame(num_na_sample)
+colnames(num_na_sample) <- "NAs"
+num_na_sample$Sample <- substr(rownames(num_na_sample), 1, 7)
+rownames(num_na_sample) <- NULL
+# Add frailty group
+num_na_sample <- merge(num_na_sample, col_data[, c("sample", "frailty")], 
+                       by.x = "Sample", by.y = "sample")
+
+# Density plot number of missing per sample in FT vs NFT
+ggplot(num_na_sample, aes(x = NAs, fill = frailty)) +
+  geom_density(alpha = 0.4, linewidth = 0.2) +
+  labs(title = "Number of NAs per sample", x = "Number of NAs", y = "Density") +
+  scale_fill_manual(values = c("FT" = "blue", "NFT" = "red")) +
+  theme_minimal()
+
+# Make binary assay (1 = valid value, 0 = missing value)
+missval <- ifelse(is.na(se_filtered_assay), 0, 1)
+colnames(missval) <- substr(colnames(missval), 1, 7)
+
+# Convert matrix to long format dataframe
+df <- melt(missval)
+
+# Rename columns
+colnames(df) <- c("protein", "sample", "intensity")
+
+# Rename samples
+df$sample <- substr(df$sample, 1, 7)
+
+# Add frailty group
+col_data <- as.data.frame(colData(se))
+df <- merge(df, col_data[, c("sample", "frailty")], by = "sample")
+
+# Plot heatmap
+ggplot(df, aes(sample, protein, fill = intensity)) + 
+  geom_tile() +
+  facet_grid(. ~ frailty, scales = "free_x", space = "free_x") +
+  theme(legend.position = "none", 
+        axis.text.x = element_text(size = 2, angle = 90),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  scale_fill_gradient(low = "white", high = "black")
+
+# Complex Heatmap
+# Clustering rows and columns by binary distance
+# Column split by frailty groups
+Heatmap(missval, name = "NA", col = c("white", "black"), show_row_dend = FALSE, 
+        column_names_gp = gpar(fontsize = 4), show_row_names = FALSE, 
+        # cluster_columns = FALSE,
+        # cluster_rows = FALSE,
+        column_split = colData(se_filtered)$frailty, 
+        clustering_distance_rows = "binary", 
+        clustering_distance_columns = "binary")
+
+# Annotation of columns by frailty group
+column_ann <- HeatmapAnnotation(frailty = colData(se_filtered)$frailty,
+                                col = list(frailty = c("FT" = "red", 
+                                                       "NFT" = "blue")))
+Heatmap(missval, name = "NA", col = c("white", "black"), show_row_dend = FALSE, 
+        column_names_gp = gpar(fontsize = 4), show_row_names = FALSE, 
+        # cluster_columns = FALSE,
+        # cluster_rows = FALSE,
+        clustering_distance_rows = "binary", 
+        clustering_distance_columns = "binary",
+        top_annotation = column_ann)
 
 # Save SummarizedExperiment filtered
 save_path <- paste0(work_path,"/data/se_filtered.RData")
 save(se_filtered, file = save_path)
 
-table(is.na(assay(se_filtered)))
 
 # ==================================
 # 5. Normalization
